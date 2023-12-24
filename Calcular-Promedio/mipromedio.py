8c24826d0a9673f9d3f080482900171e019e781f
Jeremy <88123726+9tiuu@users.noreply.github.com>
2023-12-24 05:14:31 -0300

    Calcular Promedio de notas

--- Calcular-Promedio/mipromedio.py
+++ Calcular-Promedio/mipromedio.py
@@ -1,5 +1,4 @@
-# BY 9tiuu
-
+# BY 9tiuu | Update code: 24/12/23
 import os
 os.system('cls')
 
@@ -14,46 +13,35 @@ try:
     print()
 
     if asd01 == 1:
+        print('\nIngresar NOTA como decimal y con PUNTO, ejemplo: 7.0')
+        print('Ingresar POCENTAJE como decimal y con PUNTO, ejemplo: 15% -> 0.15')
 
-        print('Ingresar NOTA como decimal y con PUNTO, ejemplo: 7.0 \nIngresar POCENTAJE como decimal y con PUNTO, ejemplo: 15% -> 0.15')
-        print()
-
-        nota1 = float(input('Nota 1: ').strip())
+        nota1 = float(input('\nNota 1: ').strip())
         porcentaje1 = float(input('Porcentaje: ').strip())
-        print()
 
-        nota2 = float(input('Nota 2: ').strip())
+        nota2 = float(input('\nNota 2: ').strip())
         porcentaje2 = float(input('Porcentaje: ').strip())
-        print()
 
-        nota3 = float(input('Nota 3: ').strip())
+        nota3 = float(input('\nNota 3: ').strip())
         porcentaje3 = float(input('Porcentaje: ').strip())
-        print()
 
-        nota4 = float(input('Nota 4: ').strip())
+        nota4 = float(input('\nNota 4: ').strip())
         porcentaje4 = float(input('Porcentaje: ').strip())
-        print()
 
         promedio = (nota1 * porcentaje1) + (nota2 * porcentaje2) + (nota3 * porcentaje3) + (nota4 * porcentaje4)
-        print(f'Promedio de asignatura: {round(promedio, 2)}')
-        print()
+        print(f'\nPromedio de asignatura: {round(promedio, 2)}\n')
 
     elif asd01 == 2:
-
         ramos = int(input('Total de asignaturas: ').strip())
-        print()
-
         cantidad_de_ramos = ramos
         contador = 1
-
         notas = []
 
         while ramos:
-            nota = float(input(f'Promedio de asignatura {contador}: ').strip())
+            nota = float(input(f'\nPromedio de asignatura {contador}: ').strip())
             notas.append(nota)
             contador += 1
             ramos -= 1
-        print()
         
         suma_de_notas = 0
 
@@ -61,13 +49,10 @@ try:
             suma_de_notas += n
 
         promedio_final = suma_de_notas / cantidad_de_ramos
-        print(f'Promedio final: {round(promedio_final, 2)}')
-        print()
+        print(f'\nPromedio final: {round(promedio_final, 2)}\n')
 
     else:
-        print('NOTA: No hay mas opciones... \nVuelve a ejecutar el programa...')
-        print()
+        print('\n> NOTA: No hay mas opciones... \nVuelve a ejecutar el programa...\n')
+        
 except:
-    print()
-    print('NOTA: Escriba alguna de las 2 opciones... \nVuelve a ejecutar el programa...')
-    print()
+    print('\n> NOTA: Escriba alguna de las 2 opciones... \nVuelve a ejecutar el programa...\n')
